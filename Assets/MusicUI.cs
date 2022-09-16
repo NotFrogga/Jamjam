d1741cef17ce99ccadaa8e616faa8a609bc6c878
Hichame <hichame.ouk@gmail.com>
2022-09-16 16:11:43 +0200

    begin ui

---
+++ Assets/MusicUI.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicUI : MonoBehaviour
+{
+    [SerializeField] public Button _tabMelodyButton;
+    [SerializeField] public Button _tabBatteryButton;
+    [SerializeField] public Button _tabBassButton;
+
+    [SerializeField] public static GameObject _selectedTabGO;
+
+    [SerializeField] private GameObject _tabMelodyGO;
+    [SerializeField] private GameObject _tabBatteryGO;
+    [SerializeField] private GameObject _tabBassGO;
+
+    private void Awake()
+    {
+        _tabMelodyGO.SetActive(true);
+        _tabBatteryGO.SetActive(false);
+        _tabBassGO.SetActive(false);
+
+        _selectedTabGO = _tabMelodyGO;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        _tabMelodyButton.onClick.AddListener(SetTabActiveMelody);
+        _tabBatteryButton.onClick.AddListener(SetTabActiveBattery);
+        _tabBassButton.onClick.AddListener(SetTabActiveBass);
+    }
+
+    private void SetTabActiveMelody()
+    {
+        _selectedTabGO.gameObject.SetActive(false);
+        _tabMelodyGO.SetActive(true);
+        _selectedTabGO = _tabMelodyGO;
+    }
+
+    private void SetTabActiveBattery()
+    {
+        _selectedTabGO.gameObject.SetActive(false);
+        _tabBatteryGO.SetActive(true);
+        _selectedTabGO = _tabBatteryGO;
+    }
+
+    private void SetTabActiveBass()
+    {
+        _selectedTabGO.gameObject.SetActive(false);
+        _tabBassGO.SetActive(true);
+        _selectedTabGO = _tabBassGO;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
