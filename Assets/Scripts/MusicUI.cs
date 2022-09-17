a40fc4bb0f272a91331ec872ed0f958c768960bb
Hichame <hichame.ouk@gmail.com>
2022-09-17 18:52:25 +0200

    ADd animations to disks

--- Assets/Scripts/MusicUI.cs
+++ Assets/Scripts/MusicUI.cs
@@ -13,6 +13,10 @@ public class MusicUI : MonoBehaviour
     public InstrumentButton _activeDrumsButton;
     public InstrumentButton _activeBassButton;
 
+    public GameObject _activeMelodyVinylImage = null;
+    public GameObject _activeDrumsVinylImage = null;
+    public GameObject _activeBassVinylImage = null;
+
     [SerializeField] private GameManager _gameManager;
 
     public void PlayInstrument(Instrument instrument)
@@ -22,16 +26,19 @@ public class MusicUI : MonoBehaviour
             case InstrumentType.MELODY:
                 if (_activeMelodyButton != null)
                     _activeMelodyButton._selectedGO.SetActive(false);
+                _activeMelodyVinylImage.GetComponent<Image>().sprite = instrument._sprite;
                 //_melodyTrack.SetInstrumentId(instrument.InstrumentId);
                 break;
             case InstrumentType.DRUMS:
                 if (_activeDrumsButton != null)
                     _activeDrumsButton._selectedGO.SetActive(false);
+                _activeDrumsVinylImage.GetComponent<Image>().sprite = instrument._sprite;
                 //_drumsTrack.SetInstrumentId(instrument.InstrumentId);
                 break;
             case InstrumentType.BASS:
                 if (_activeBassButton != null)
                     _activeBassButton._selectedGO.SetActive(false);
+                _activeBassVinylImage.GetComponent<Image>().sprite = instrument._sprite;
                 //_bassTrack.SetInstrumentId(instrument.InstrumentId);
                 break;
             default:
