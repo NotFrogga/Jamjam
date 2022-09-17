6f79210c7c0371e9d61d88053a8e3090382339e5
Hichame <hichame.ouk@gmail.com>
2022-09-17 19:07:46 +0200

    stop animation when mute

--- Assets/Scripts/VinylAnimationHandler.cs
+++ Assets/Scripts/VinylAnimationHandler.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class VinylAnimationHandler : MonoBehaviour
 {
+    public AudioTrack _audioTrack;
+    Animator _animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_audioTrack != null && _audioTrack.ID == 0)
+        {
+            _animator.speed = 0;
+        }
+        else
+        {
+            _animator.speed = 1;
+        }
+
     }
+
+
 }
